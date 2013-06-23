4282129e67f089a607428afdec4bcbb0fd9198c6
Andy Vandijck <andyvand@iMac-van-Andy.local>
2013-06-23 20:35:53 +0200

    Initial Commit

---
+++ FixEDID/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FixEDID
+//
+//  Created by Andy Vandijck on 23/06/13.
+//  Copyright (c) 2013 Andy Vandijck. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FixEDID/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  FixEDID
+//
+//  Created by Andy Vandijck on 23/06/13.
+//  Copyright (c) 2013 Andy Vandijck. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
